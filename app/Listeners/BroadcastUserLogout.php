79936fa9780db15583c9e8d85566ab859f136023
jeanmg26 <jeanmg25@gmail.com>
2021-10-06 18:01:26 -0500

    chatbox-office-06102021-1801

---
+++ app/Listeners/BroadcastUserLogout.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserSessionChanged;
+use Illuminate\Auth\Events\Logout;
+
+class BroadcastUserLogout
+{
+   /**
+    * Create the event listener.
+    *
+    * @return void
+    */
+   public function __construct()
+   {
+      //
+   }
+
+   /**
+    * Handle the event.
+    *
+    * @param  object  $event
+    * @return void
+    */
+   public function handle(Logout $event)
+   {
+      broadcast(new UserSessionChanged($event->user, 'danger'));
+
+   }
+}
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@push('styles')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+
+@endpush
+
+@section('content')
+<div class="container">
+   <div class="row justify-content-center">
+      <div class="col-md-8">
+         <div class="card">
+            <div class="card-header">Usuarios</div>
+            <div class="card-body">
+               <ul id="users" class="list-unstyled"></ul>
+
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+@endsection
+
+@push('scripts')
+<script>
+   axios.get('api/users')
+   .then( (resp) => {
+
+      const usersElement = document.getElementById('users');
+      let users = resp.data;
+
+      users.forEach( (user, index) => {
+         
+         let element = document.createElement('li');
+         let link =  document.createElement('a');
+         let icon = document.createElement('i')
+
+         link.className  = 'text-decoration-none';
+         link.setAttribute('href', 'javascript::void(0)');
+         icon.className = 'fas fa-circle me-2 text-danger';
+         icon.setAttribute('id', 'status_'+user.id);
+         link.innerText = user.name;
+         link.prepend(icon);
+         element.append(link);
+         usersElement.appendChild(element);
+      });
+      
+   });
+
+   // Usuarios logueados
+
+   Echo.private('notifications')
+      .listen('UserSessionChanged', (e) => {
+         
+         let userStatus = document.getElementById('status_'+e.user.id);
+         
+         userStatus.classList.remove('text-danger');
+         userStatus.classList.remove('text-sucess');
+         userStatus.classList.add('text-'+e.type);
+      });
+
+
+
+
+</script>
+@endpush
